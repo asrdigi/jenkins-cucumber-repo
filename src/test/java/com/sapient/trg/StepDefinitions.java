11ca593edee90f76f6ecd3bb1046fb4c761d3fe4
Annapureddy Srinivas Reddy <digitech1993@gmail.com>
2021-08-09 11:32:17 +0530

    my commit2

--- src/test/java/com/sapient/trg/StepDefinitions.java
+++ src/test/java/com/sapient/trg/StepDefinitions.java
@@ -6,16 +6,9 @@ import io.cucumber.java.en.Then;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class IsItFriday {
-	//    static String isItFriday(String today) {
-	//        return null;
-	//    }
-	
-//	static String isItFriday(String today) {
-//		return "Nope";
-//	}
-	
+
 	static String isItFriday(String today) {
-	    return "Friday".equals(today) ? "TGIF" : "Nope";
+		return "Friday".equals(today) ? "TGIF" : "Nope";
 	}
 
 }
@@ -24,38 +17,42 @@ public class StepDefinitions {
 	private String today;
 	private String actualAnswer;
 
-//	@Given("today is Sunday")
-//	public void today_is_Sunday() {
-//		today = "Sunday";
-//	}
-//
-//	@When("I ask whether it's Friday yet")
-//	public void i_ask_whether_it_s_Friday_yet() {
-//		actualAnswer = IsItFriday.isItFriday(today);
-//	}
-//
-//	@Then("I should be told {string}")
-//	public void i_should_be_told(String expectedAnswer) {
-//		assertEquals(expectedAnswer, actualAnswer);
-//	}
-//	
-//	@Given("today is Friday")
-//	public void today_is_Friday() {
-//	    today = "Friday";
-//	}
-	
+	//		@Given("today is Sunday")
+	//		public void today_is_Sunday() {
+	//			today = "Sunday";
+	//		}	
+	//		
+	//		@Given("today is Friday")
+	//		public void today_is_Friday() {
+	//		    today = "Friday";
+	//		}
+	//		
+	//		@When("I ask whether it's Friday yet")
+	//		public void i_ask_whether_it_s_Friday_yet() {
+	//			actualAnswer = IsItFriday.isItFriday(today);
+	//		}
+	//	
+	//		
+	//		@Then("I should be told {string}")
+	//		public void i_should_be_told(String expectedAnswer) {
+	//			assertEquals(expectedAnswer, actualAnswer);
+	//		}		
+
+
+
+
 	@Given("today is {string}")
-    public void today_is(String today) {
-        this.today = today;
-    }
+	public void today_is(String today) {
+		this.today = today;
+	}
 
-    @When("I ask whether it's Friday yet")
-    public void i_ask_whether_it_s_Friday_yet() {
-        actualAnswer = IsItFriday.isItFriday(today);
-    }
+	@When("I ask whether it's Friday yet")
+	public void i_ask_whether_it_s_Friday_yet() {
+		actualAnswer = IsItFriday.isItFriday(today);
+	}
 
-    @Then("I should be told {string}")
-    public void i_should_be_told(String expectedAnswer) {
-        assertEquals(expectedAnswer, actualAnswer);
-    }
+	@Then("I should be told {string}")
+	public void i_should_be_told(String expectedAnswer) {
+		assertEquals(expectedAnswer, actualAnswer);
+	}
 }
