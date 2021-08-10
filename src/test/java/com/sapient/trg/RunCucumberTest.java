74eb90e26c8c9e8accc2cfaf7686c39c5b1441ce
Annapureddy Srinivas Reddy <digitech1993@gmail.com>
2021-08-10 12:05:35 +0530

    latest112

--- src/test/java/com/sapient/trg/RunCucumberTest.java
+++ src/test/java/com/sapient/trg/RunCucumberTest.java
@@ -4,10 +4,8 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.platform.engine.Cucumber;
 
 @Cucumber
-@CucumberOptions(
-		features = "classpath:features",		
-		plugin = {"pretty", "json:target/cucumber-report.json"}
-		)
+@CucumberOptions(features = "classpath:features", plugin = {"pretty",
+"json:target/cucumber-report.json"})
 public class RunCucumberTest {
 
 }
